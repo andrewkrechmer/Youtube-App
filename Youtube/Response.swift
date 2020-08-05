11906c141537c8cc106be4a6d9077a31ff3a4486
Andrew Krechmer <andrew.krechmer@gmail.com>
2020-08-05 13:27:50 -0400

    added code to parse the json responce

---
+++ Youtube/Response.swift
@@ -0,0 +1,28 @@
+//
+//  Response.swift
+//  Youtube
+//
+//  Created by Andrew Krechmer on 2020-08-05.
+//  Copyright © 2020 Andrew Krechmer. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items:[Video]?
+    
+    enum CodingKeys:String, CodingKey {
+        
+        case items
+        
+    }
+    
+    init (from decoder:Decoder) throws {
+    
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+        
+    }
+}
